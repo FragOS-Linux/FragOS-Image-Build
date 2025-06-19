bf14c0b9595152e54905665d8cdb71faf2de354e
krolmiki2011 <mikolajziolkowski504@gmail.com>
2025-06-19 20:41:43 +0200

    Reset

--- build-image.sh
+++ build-image.sh
@@ -1,36 +1,18 @@
-#!/bin/bash
+#! /bin/bash
 
 set -e
-
-BUILD_USER=${BUILD_USER:-}
-OUTPUT_DIR=${OUTPUT_DIR:-}
-
+set -x
 
 if [ $EUID -ne 0 ]; then
 	echo "$(basename $0) must be run as root"
 	exit 1
 fi
 
+BUILD_USER=${BUILD_USER:-}
+OUTPUT_DIR=${OUTPUT_DIR:-}
 
-VERSION="1-Beta"
-SYSTEM_DESC="FragOS"
-SYSTEM_NAME="fragos"
-USERNAME="gamer"
-SIZE="12000MB"
-ARCHIVE_DATE=$(date -d 'yesterday' +%Y/%m/%d)
-WEBSITE="https://fragos.github.io"
-KERNEL_PACKAGE="linux"
-PACKAGES=$(grep -vE '^\s*#|^\s*$' packages.conf | tr '\n' ' ')
-SERVICES=$(cat services-to-enable.conf | tr -d '"' | tr -d '\\' | tr '\n' ' ')
-USER_SERVICES=$(cat services-to-enable-user.conf | tr -d '"' | tr -d '\\' | tr '\n' ' ')
-FILES_TO_DELETE="\
-	/boot/initramfs-linux-fallback.img \
-	/usr/share/SFML \
-	/usr/share/doc \
-	/usr/share/gtk-doc \
-	/usr/share/help \
-	/usr/share/man \
-"
+
+source manifest
 
 if [ -z "${SYSTEM_NAME}" ]; then
   echo "SYSTEM_NAME must be specified"
@@ -65,21 +47,32 @@ mkfs.btrfs -f ${BUILD_IMG}
 mount -t btrfs -o loop,compress-force=zstd:15 ${BUILD_IMG} ${MOUNT_PATH}
 btrfs subvolume create ${BUILD_PATH}
 
+# copy the makepkg.conf into chroot
+cp /etc/makepkg.conf rootfs/etc/makepkg.conf
 
-echo FRAGOS SYSTEM BUILDER...
-
-echo STAGE 1: Downloading rootfs from pacstrap...
-
+# bootstrap using our configuration
 pacstrap -K -C rootfs/etc/pacman.conf ${BUILD_PATH}
 
-echo STAGE 2: Configuring rootfs...
+# copy the builder mirror list into chroot
 mkdir -p rootfs/etc/pacman.d
 cp /etc/pacman.d/mirrorlist rootfs/etc/pacman.d/mirrorlist
 
+# copy files into chroot
+cp -R manifest rootfs/. ${BUILD_PATH}/
+
+mkdir ${BUILD_PATH}/flatpak_pkgs
+mkdir ${BUILD_PATH}/aur_pkgs
+
+cp -rv aur-pkgs/*.pkg.tar* ${BUILD_PATH}/aur_pkgs
+
+
+# chroot into target
 mount --bind ${BUILD_PATH} ${BUILD_PATH}
 arch-chroot ${BUILD_PATH} /bin/bash <<EOF
 set -e
 set -x
+
+source /manifest
 
 pacman-key --populate
 
@@ -92,41 +85,71 @@ sed -i '/ParallelDownloads/s/^/#/g' /etc/pacman.conf
 # Cannot check space in chroot
 sed -i '/CheckSpace/s/^/#/g' /etc/pacman.conf
 
+# update package databases
 pacman --noconfirm -Syy
 
+# Disable check and debug for makepkg on the final image
 sed -i '/BUILDENV/s/ check/ !check/g' /etc/makepkg.conf
 sed -i '/OPTIONS/s/ debug/ !debug/g' /etc/makepkg.conf
 
-pacman --noconfirm -S "${KERNEL_PACKAGE}" "${KERNEL_PACKAGE}-headers"
+# install kernel package
+if [ "$KERNEL_PACKAGE_ORIGIN" == "local" ] ; then
+	pacman --noconfirm -U --overwrite '*' \
+	/override_pkgs/${KERNEL_PACKAGE}-*.pkg.tar.zst
+else
+	pacman --noconfirm -S "${KERNEL_PACKAGE}" "${KERNEL_PACKAGE}-headers"
+fi
 
+# install flatpak packages
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub ${FLATPAK_PACKAGES}
+
+# remove jack2 to prevent conflict with pipewire-jack
 pacman --noconfirm -Rdd jack2 || true
 
+# install packages
 pacman --noconfirm -S --overwrite '*' --disable-download-timeout ${PACKAGES}
 rm -rf /var/cache/pacman/pkg
 
+# install AUR packages
+pacman --noconfirm -U --overwrite '*' /aur_pkgs/*
+rm -rf /var/cache/pacman/pkg
+
+# Install the new iptables
+# See https://gitlab.archlinux.org/archlinux/packaging/packages/iptables/-/issues/1
+# Since base package group adds iptables by default
+# pacman will ask for confirmation to replace that package
+# but the default answer is no.
+# doing yes | pacman omitting --noconfirm is a necessity
 yes | pacman -S iptables-nft
 
+# enable services
 systemctl enable ${SERVICES}
 
+# enable user services
 systemctl --global enable ${USER_SERVICES}
 
+# disable root login
 passwd --lock root
 
+# create user
 groupadd -r autologin
 useradd -m ${USERNAME} -G autologin,wheel,plugdev
 echo "${USERNAME}:${USERNAME}" | chpasswd
 
+# set the default editor, so visudo works
 echo "export EDITOR=/usr/bin/vim" >> /etc/bash.bashrc
 
-#Set autologin
+# Set SDDM AutoLogin
 mkdir -p /etc/sddm.conf.d
-
 echo "[Autologin]
 User=${USERNAME}
-Session=plasma.desktop" > /etc/sddm.conf.d/autologin.conf
+Session=plasma.desktop" > /etc/sddm.conf.d/00-autologin.conf
+
 
 echo "${SYSTEM_NAME}" > /etc/hostname
 
+# enable multicast dns in avahi
 sed -i "/^hosts:/ s/resolve/mdns resolve/" /etc/nsswitch.conf
 
 # configure ssh
@@ -140,10 +163,10 @@ Subsystem	sftp	/usr/lib/ssh/sftp-server
 " > /etc/ssh/sshd_config
 
 echo "
-LABEL=frzr_root /var       btrfs     defaults,subvolid=256,rw,noatime,nodatacow,nofail                                                                                                                                                                                                                      0   0
-LABEL=frzr_root /home      btrfs     defaults,subvolid=257,rw,noatime,nodatacow,nofail                                                                                                                                                                                                                      0   0
-LABEL=frzr_root /frzr_root btrfs     defaults,subvolid=5,rw,noatime,nodatacow,x-initrd.mount                                                                                                                                                                                                                0   2
-overlay         /etc       overlay   defaults,x-systemd.requires-mounts-for=/frzr_root,x-systemd.requires-mounts-for=/sysroot/frzr_root,x-systemd.rw-only,lowerdir=/sysroot/etc,upperdir=/sysroot/frzr_root/etc,workdir=/sysroot/frzr_root/.etc,index=off,metacopy=off,comment=etcoverlay,x-initrd.mount    0   0
+LABEL=zenkai_root /var       btrfs     defaults,subvolid=256,rw,noatime,nodatacow,nofail                                                                                                                                                                                                                      0   0
+LABEL=zenkai_root /home      btrfs     defaults,subvolid=257,rw,noatime,nodatacow,nofail                                                                                                                                                                                                                      0   0
+LABEL=zenkai_root /zenkai_root btrfs     defaults,subvolid=5,rw,noatime,nodatacow,x-initrd.mount                                                                                                                                                                                                                0   2
+overlay         /etc       overlay   defaults,x-systemd.requires-mounts-for=/zenkai_root,x-systemd.requires-mounts-for=/sysroot/zenkai_root,x-systemd.rw-only,lowerdir=/sysroot/etc,upperdir=/sysroot/zenkai_root/etc,workdir=/sysroot/zenkai_root/.etc,index=off,metacopy=off,comment=etcoverlay,x-initrd.mount    0   0
 " > /etc/fstab
 
 echo "
@@ -159,9 +182,19 @@ VERSION_ID="${VERSION_NUMBER}"
 BUILD_ID="${BUILD_ID}"
 PRETTY_NAME="${SYSTEM_DESC} ${DISPLAY_VERSION}"
 ID=${SYSTEM_NAME}
-ID_LIKE=arch' > /usr/lib/os-release
+ID_LIKE=arch
+ANSI_COLOR="1;31"
+HOME_URL="${WEBSITE}"
+DOCUMENTATION_URL="${DOCUMENTATION_URL}"
+BUG_REPORT_URL="${BUG_REPORT_URL}"' > /usr/lib/os-release
 
+# run post install hook
+postinstallhook
 
+# record installed packages & versions
+pacman -Q > /manifest
+
+# preserve installed package database
 mkdir -p /usr/var/lib/pacman
 cp -r /var/lib/pacman/local /usr/var/lib/pacman/
 
@@ -172,11 +205,9 @@ if [ ${KERNEL_PACKAGE} != 'linux' ] ; then
 	mv /boot/initramfs-${KERNEL_PACKAGE}-fallback.img /boot/initramfs-linux-fallback.img
 fi
 
+# clean up/remove unnecessary files
 rm -rf \
-/local_pkgs \
 /aur_pkgs \
-/override_pkgs \
-/extra_certs \
 /home \
 /var \
 
@@ -185,19 +216,23 @@ rm -rf ${FILES_TO_DELETE}
 # create necessary directories
 mkdir -p /home
 mkdir -p /var
-mkdir -p /frzr_root
+mkdir -p /zenkai_root
 mkdir -p /efi
 EOF
 
+#defrag the image
 btrfs filesystem defragment -r ${BUILD_PATH}
 
 # copy files into chroot again
 cp -R rootfs/. ${BUILD_PATH}/
-rm -rf ${BUILD_PATH}/extra_certs
 
 echo "${SYSTEM_NAME}-${VERSION}" > ${BUILD_PATH}/build_info
 echo "" >> ${BUILD_PATH}/build_info
+cat ${BUILD_PATH}/manifest >> ${BUILD_PATH}/build_info
+rm ${BUILD_PATH}/manifest
 
+# freeze archive date of build to avoid package drift on unlock
+# if no archive date is set
 if [ -z "${ARCHIVE_DATE}" ]; then
 	export TODAY_DATE=$(date +%Y/%m/%d)
 	echo "Server=https://archive.archlinux.org/repos/${TODAY_DATE}/\$repo/os/\$arch" > \
@@ -207,6 +242,9 @@ fi
 btrfs subvolume snapshot -r ${BUILD_PATH} ${SNAP_PATH}
 btrfs send -f ${SYSTEM_NAME}-${VERSION}.img ${SNAP_PATH}
 
+cp ${BUILD_PATH}/build_info build_info.txt
+
+# clean up
 umount -l ${BUILD_PATH}
 umount -l ${MOUNT_PATH}
 rm -rf ${MOUNT_PATH}
